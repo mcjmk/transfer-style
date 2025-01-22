04eaa9e484a1ce94db9e9a81f8593f60dd952d15
pmocko <pmocko@student.agh.edu.pl>
2025-01-22 21:52:28 +0100

    new ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
-import React, { StrictMode } from 'react'; 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import ReactDOM from 'react-dom/client';
-import App from './App';
- 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./style.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 root.render(
