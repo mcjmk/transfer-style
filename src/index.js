faa17fa9b1bfc3da354fe56aa15e9207331bc4f5
mcjmk <mcj.mezyk@gmail.com>
2025-01-22 17:03:55 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
